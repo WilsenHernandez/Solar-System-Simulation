eee1fc07f3b29b58b6f622c9ced425623cf72478
Wilsen Hernandez <wilsenh95@gmail.com>
2016-07-29 05:26:50 -0400

    Tidy up

--- Assets/Scripts/LookAtTarget.cs
+++ Assets/Scripts/LookAtTarget.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LookAtTarget : MonoBehaviour {
+public class LookAtTarget : MonoBehaviour
+{
 
-	static public GameObject target; // the target that the camera should look at
+    static public GameObject target; // the target that the camera should look at
+    public GameObject startingObject;
 
-	void Start () {
-		if (target == null) 
-		{
-			target = this.gameObject;
-			Debug.Log ("LookAtTarget target not specified. Defaulting to parent GameObject");
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (target)
-			transform.LookAt(target.transform);
-	}
+    void Start ()
+    {
+        if (target == null)
+        {
+            target = this.gameObject;
+            Debug.Log ("LookAtTarget target not specified. Defaulting to parent GameObject");
+        }
+
+        if (startingObject != null)
+            transform.LookAt(startingObject.transform);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        if (target)
+            transform.LookAt(target.transform);
+    }
 }
